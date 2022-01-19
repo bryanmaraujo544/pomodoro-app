f5422613a155838575782c1a1ee192a86770db77
Bryan Martins <bryanmaraujo544@gmail.com>
2022-01-19 15:01:49 -0300

    feat: adding the timer design and increasing and diminishing the pomodoro timer

--- src/utils/showNotification.ts
+++ src/utils/showNotification.ts
@@ -2,14 +2,17 @@ interface Props {
   (title?: string, bodymsg?: string, iconPath?: string): number,
 }
 
+// import 
+
+
 export const showNotification: Props = (
   title = 'Notification',
   bodyMsg = 'This is one notification',
-  iconPath = '../public/images/logo.png',
+  iconPath = '../../public/images/logo.png',
 ) => {
   const options = {
     body: bodyMsg,
-    icon: iconPath,
+    icon: "https://ps.w.org/wp-notification-bell/assets/icon-256x256.png?rev=2439614",
   };
 
   const notification = new Notification(title, options);
